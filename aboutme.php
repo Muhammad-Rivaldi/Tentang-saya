587a9e245ad7e110d025fd67a4e0543dc647eccf
Muhammad-Rivaldi <63339492+Muhammad-Rivaldi@users.noreply.github.com>
2020-04-09 01:06:17 +0700

    tentang saya

---
+++ aboutme.php
@@ -0,0 +1,7 @@
+<?php 
+    header('Content-type: text/plain');
+    echo "Nama : Muhammad Rivaldi \n";
+    echo "Kelas : XI RPL 1 \n";
+    echo "Asal Sekolah : SMK Taruna Bhakti \n";
+    echo "Alamat : Jl. Kawi 2 Kel. Abadijaya Kec. Sukmajaya";
+?>
